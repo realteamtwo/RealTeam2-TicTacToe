6f77ea8f99a7a4bd990af65630937048b22e5e47
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2024-09-14 16:42:59 -0400

    Prof added a comment

--- RealTeam2-TicTacToe/RealTeam2-TicTacToe.cpp
+++ RealTeam2-TicTacToe/RealTeam2-TicTacToe.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "This is Joseph!\n";
+    // comment from the prof
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
