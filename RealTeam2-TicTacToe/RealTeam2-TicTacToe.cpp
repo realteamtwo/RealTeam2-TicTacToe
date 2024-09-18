12940ab2069944e0148b3ec6a1843f2d0ff71e6e
boonsackly <boonpesky@gmail.com>
2024-09-18 14:04:18 -0400

    Fixed Draw Condition to check if every space is filled first.

--- RealTeam2-TicTacToe/RealTeam2-TicTacToe.cpp
+++ RealTeam2-TicTacToe/RealTeam2-TicTacToe.cpp
@@ -151,7 +151,8 @@ void checkWin(char board[3][3])
     {
         cout << board[0][0] << " wins!";
     }
-    else
+    else if (board[0][0] != ' ' && board[0][1] != ' ' && board[0][2] != ' ' && board[1][0] != ' ' && board[1][1] != ' '
+        && board[1][2] != ' ' && board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ')
     {
         cout << "Draw!";
     }
